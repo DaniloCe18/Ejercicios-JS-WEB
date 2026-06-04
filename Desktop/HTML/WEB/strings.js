1cdc996493105c3c2a8390e4e60ef1770ca8a1b1
Danilo <danilocerasa18@gmail.com>
2026-06-04 10:00:10 -0300

    Agregados los archivos de ejercicios de JS

---
+++ Desktop/HTML/WEB/strings.js
@@ -0,0 +1,4 @@
+//1
+var textoOriginal = "rey de copas";
+var textoMayuscula = textoOriginal.toUpperCase();
+console.log("2a. En mayusculas: "+ textoMayuscula);
---
+++ Desktop/HTML/WEB/variables_and_operators.js
@@ -0,0 +1,27 @@
+//1
+var num1 = 7;
+var num2 =11;
+
+function suma(num1,num2)
+{
+    var resultado = num1 + num2;
+    return resultado;
+}
+
+console.log("1a. El resultado de la suma es: " + suma(num1, num2));
+
+//2
+var texto1 = "Hola";
+var texto2 = "Mundo";
+
+var resultadoContatenacion = texto1 + texto2;
+console.log("1b. Resultado de la concatenacion: "+ resultadoContatenacion);
+
+//3
+var palabra1 = "JavaScript";
+var palabra2 = "Codigo";
+resultadoSumaLargos = palabra1.length + palabra2.length;
+
+console.log("1c. la suma del largo de ambas palabras es: " + resultadoSumaLargos);
+
+
